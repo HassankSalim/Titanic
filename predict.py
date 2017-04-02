4e1d23888d79e3aafd0ac9b853cd7a04cc42eedc
Hassan K Salim <hassanksalim@gmail.com>
2017-04-02 09:44:56 +0530

    Create pickle file for svm, add predict.py, rename svm_normalized to norm_svm

---
+++ predict.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from preprocess import processNumData, processCatData, divideDataset, get_X_y
+from time import time
+import warnings
+from sys import argv
+from pickle import load
+warnings.filterwarnings("ignore")
+
+test = pd.read_csv('test.csv')
+pickle_file_name = argv[1]
+output_csv_name = argv[2]
+
+processed_test_data = np.hstack((processCatData(test), processNumData(test)))
+
+pickle_file = open(pickle_file_name, 'rb')
+clf = load(pickle_file)
+
+pred = clf.predict(processed_test_data)
+print(pred)
+pred = pd.DataFrame(pred)
+pred.columns = ['Survived']
+
+submission = pd.concat([test['PassengerId'], pred], axis=1)
+submission.to_csv(output_csv_name, index=False)
--- train_svm.py
+++ train_svm.py
@@ -4,8 +4,18 @@ from sklearn.metrics import accuracy_score
 import pandas as pd
 import numpy as np
 from preprocess import processNumData, processCatData, divideDataset, get_X_y
+from time import time
 import warnings
+from sys import argv
+from pickle import dump
 warnings.filterwarnings("ignore")
+
+
+if len(argv) < 2:
+    print('Usage : python train_svm.py file_name.pickle')
+    exit()
+else:
+    pickle_file_name = argv[1]
 
 def replace_zero_neg_one(val):
     if val:
@@ -26,13 +36,22 @@ y.apply(lambda x : replace_zero_neg_one(x))
 processed_train_data = np.hstack((processCatData(X), processNumData(X)))
 processed_test_data = np.hstack((processCatData(test), processNumData(test)))
 
-clf = SVC(kernel='rbf', C=10, gamma='auto')
+params = { 'kernel' : ('linear', 'rbf'), 'C' : [1, 10, 100, 1000, 10000] }
+clf = SVC(kernel='rbf', C=10, gamma='auto') #svc
+
+# t = time()
+# print('Start.....')
+# clf = GridSearchCV(estimator = svc, param_grid = params, cv = 10, n_jobs=-1)
 clf.fit(processed_train_data, y)
+with open(pickle_file_name, 'wb') as f:
+    dump(clf, f)
+# print clf
+# print('End.......')
+# print('Time Elasped : ', time() - t)
 
-pred = clf.predict(processed_test_data)
-pred = pd.DataFrame(pred)
-pred.columns = ['Survived']
-
-submission = pd.concat([test['PassengerId'], pred], axis=1)
-submission.to_csv('svm_normalized.csv', index=False)
-print 'End'
+# pred = clf.predict(processed_test_data)
+# pred = pd.DataFrame(pred)
+# pred.columns = ['Survived']
+#
+# submission = pd.concat([test['PassengerId'], pred], axis=1)
+# submission.to_csv('grid_search_cv_svm_normalized.csv', index=False)
